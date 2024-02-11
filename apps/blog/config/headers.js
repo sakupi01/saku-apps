7a77fa61b82c03f6c11dc83aa35a7487a61a8f12
saku-1101 <sakupi1101@outlook.jp>
2024-02-11 21:15:02 +0900

    chore: http headers

---
+++ apps/blog/config/headers.js
@@ -0,0 +1,40 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: `frame-ancestors 'self' https://app.contentful.com https://app.eu.contentful.com`,
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "no-referrer",
+  },
+];
+
+module.exports = async () => {
+  return [
+    {
+      // Apply these headers to all routes in your application.
+      source: "/:path*",
+      headers: securityHeaders,
+    },
+  ];
+};
--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,4 +1,6 @@
+import headers from "./config/headers";
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  headers,
 };
