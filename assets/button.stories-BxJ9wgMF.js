5296692d33e0b1ec9dfb97ff078256355b39c663
saku-1101 <saku-1101@users.noreply.github.com>
2024-02-11 09:42:41 +0000

    deploy: 5f455afa4d07ed316feda82e5fe2d54233e0ce63

---
+++ assets/button.stories-BxJ9wgMF.js
@@ -0,0 +1,6 @@
+import{j as i}from"./jsx-runtime-DRTy3Uxn.js";import"./index-BBkUAzwr.js";const s=({children:o,className:n,appName:p})=>i.jsx("button",{type:"submit",className:n,onClick:()=>alert(`Hello from your ${p} app!`),children:o});s.__docgenInfo={description:"",methods:[],displayName:"Button",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},appName:{required:!0,tsType:{name:"string"},description:""}}};const m={title:"Button",component:s,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},e={args:{children:null,appName:"app"}};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    children: null,
+    appName: "app"
+  }
+}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,m as default};
