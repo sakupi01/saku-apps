b4cddeac7cd1172a3d853c69cfc6907febdbffa3
saku-1101 <sakupi1101@outlook.jp>
2024-02-12 16:23:16 +0900

    chore: format

--- apps/blog/@types/contentful/TypePageLanding.ts
+++ apps/blog/@types/contentful/TypePageLanding.ts
@@ -1,12 +1,24 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
 import type { TypeComponentSeoSkeleton } from "./TypeComponentSeo";
 import type { TypePageBlogPostSkeleton } from "./TypePageBlogPost";
 
 export interface TypePageLandingFields {
-    internalName: EntryFieldTypes.Symbol;
-    seoFields?: EntryFieldTypes.EntryLink<TypeComponentSeoSkeleton>;
-    featuredBlogPost?: EntryFieldTypes.EntryLink<TypePageBlogPostSkeleton>;
+  internalName: EntryFieldTypes.Symbol;
+  seoFields?: EntryFieldTypes.EntryLink<TypeComponentSeoSkeleton>;
+  featuredBlogPost?: EntryFieldTypes.EntryLink<TypePageBlogPostSkeleton>;
 }
 
-export type TypePageLandingSkeleton = EntrySkeletonType<TypePageLandingFields, "pageLanding">;
-export type TypePageLanding<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypePageLandingSkeleton, Modifiers, Locales>;
+export type TypePageLandingSkeleton = EntrySkeletonType<
+  TypePageLandingFields,
+  "pageLanding"
+>;
+export type TypePageLanding<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode,
+> = Entry<TypePageLandingSkeleton, Modifiers, Locales>;
