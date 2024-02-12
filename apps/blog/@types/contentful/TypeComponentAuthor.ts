38e92aab4b448bb217baae7aad719ab889fb4073
saku-1101 <sakupi1101@outlook.jp>
2024-02-12 16:23:07 +0900

    chore: ignore types format

--- apps/blog/@types/contentful/TypeComponentAuthor.ts
+++ apps/blog/@types/contentful/TypeComponentAuthor.ts
@@ -1,10 +1,22 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
 
 export interface TypeComponentAuthorFields {
-    internalName: EntryFieldTypes.Symbol;
-    name: EntryFieldTypes.Symbol;
-    avatar?: EntryFieldTypes.AssetLink;
+  internalName: EntryFieldTypes.Symbol;
+  name: EntryFieldTypes.Symbol;
+  avatar?: EntryFieldTypes.AssetLink;
 }
 
-export type TypeComponentAuthorSkeleton = EntrySkeletonType<TypeComponentAuthorFields, "componentAuthor">;
-export type TypeComponentAuthor<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypeComponentAuthorSkeleton, Modifiers, Locales>;
+export type TypeComponentAuthorSkeleton = EntrySkeletonType<
+  TypeComponentAuthorFields,
+  "componentAuthor"
+>;
+export type TypeComponentAuthor<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode,
+> = Entry<TypeComponentAuthorSkeleton, Modifiers, Locales>;
--- apps/blog/@types/contentful/index.ts
+++ apps/blog/@types/contentful/index.ts
@@ -1,5 +1,25 @@
-export type { TypeComponentAuthor, TypeComponentAuthorFields, TypeComponentAuthorSkeleton } from "./TypeComponentAuthor";
-export type { TypeComponentRichImage, TypeComponentRichImageFields, TypeComponentRichImageSkeleton } from "./TypeComponentRichImage";
-export type { TypeComponentSeo, TypeComponentSeoFields, TypeComponentSeoSkeleton } from "./TypeComponentSeo";
-export type { TypePageBlogPost, TypePageBlogPostFields, TypePageBlogPostSkeleton } from "./TypePageBlogPost";
-export type { TypePageLanding, TypePageLandingFields, TypePageLandingSkeleton } from "./TypePageLanding";
+export type {
+  TypeComponentAuthor,
+  TypeComponentAuthorFields,
+  TypeComponentAuthorSkeleton,
+} from "./TypeComponentAuthor";
+export type {
+  TypeComponentRichImage,
+  TypeComponentRichImageFields,
+  TypeComponentRichImageSkeleton,
+} from "./TypeComponentRichImage";
+export type {
+  TypeComponentSeo,
+  TypeComponentSeoFields,
+  TypeComponentSeoSkeleton,
+} from "./TypeComponentSeo";
+export type {
+  TypePageBlogPost,
+  TypePageBlogPostFields,
+  TypePageBlogPostSkeleton,
+} from "./TypePageBlogPost";
+export type {
+  TypePageLanding,
+  TypePageLandingFields,
+  TypePageLandingSkeleton,
+} from "./TypePageLanding";
