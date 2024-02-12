a7da3b9a4bcd613a39c1afd9702c30b41c945d37
saku-1101 <sakupi1101@outlook.jp>
2024-02-12 09:58:42 +0900

    chore: apply ui components to app

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -4,12 +4,7 @@ export default async function Page() {
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center p-24">
       <Gradient className="opacity-5 w-[120px] h-[120px]" conic small />
-      <Button
-        appName="web"
-        className="px-4 py-2 text-white bg-blue-500 rounded-md shadow-md"
-      >
-        Click me!
-      </Button>
+      <Button appName="web">Click me!</Button>
 
       <Test />
 
