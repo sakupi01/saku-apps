dcc974f86052faa25943d49bb4ca4921bde5b4f2
saku-1101 <sakupi1101@outlook.jp>
2024-02-13 23:29:39 +0900

    feat: HTMLをDOMとして読み込ませてスタイル当てる

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,15 +1,21 @@
 import { getAllArticles } from "@/libs/getApi";
 import { Button } from "@repo/ui";
+import Link from "next/link";
 
 export default async function Page() {
-  const content = getAllArticles();
-  console.log(content);
-
+  const allArticles = getAllArticles();
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center p-24">
       <Button appName="web">Click me!</Button>
 
-      <p>blog listing goes here</p>
+      <h1>Articles</h1>
+      {allArticles.map((article) => (
+        <Link href={`/articles/${article.slug}`}>
+          <div key={article.slug}>
+            <h2>{article.title}</h2>
+          </div>
+        </Link>
+      ))}
     </main>
   );
 }
