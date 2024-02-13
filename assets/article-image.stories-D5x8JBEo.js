a37eeee24966de73de3137750d047d63c309d701
saku-1101 <saku-1101@users.noreply.github.com>
2024-02-13 12:34:47 +0000

    deploy: 457573cf8861c60ad0895f5bcf9caea3f21294e1

---
+++ assets/article-image.stories-D5x8JBEo.js
@@ -0,0 +1,3 @@
+import{A as o}from"./article-image-D-E7h-Ga.js";import"./clsx-B-dksMZM.js";import"./ctf-image-BW9QFbOY.js";import"./index-BBkUAzwr.js";import"./index-PqR-_bA4.js";const l={title:"ArticleImage",component:o,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},r={args:{}};var e,t,a;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {}
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const n=["Default"];export{r as Default,n as __namedExportsOrder,l as default};
