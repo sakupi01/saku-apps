b2002a3f9254b5eb660141b49bc7a8b75ed45824
saku-1101 <sakupi1101@outlook.jp>
2024-02-13 22:45:21 +0900

    feat: markdownの内容をとってくるとこまで

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,11 +1,13 @@
-import { Button, Test } from "@repo/ui";
+import { getAllArticles } from "@/libs/getApi";
+import { Button } from "@repo/ui";
 
 export default async function Page() {
+  const content = getAllArticles();
+  console.log(content);
+
   return (
     <main className="flex min-h-screen min-w-screen flex-col items-center justify-center p-24">
       <Button appName="web">Click me!</Button>
-
-      <Test />
 
       <p>blog listing goes here</p>
     </main>
