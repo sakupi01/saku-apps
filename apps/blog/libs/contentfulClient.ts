8ba74ca15fb7d2dc3a9ed2deefc32583a1fd6c9f
saku-1101 <sakupi1101@outlook.jp>
2024-02-13 11:06:19 +0900

    feat: 静的に生成したrouteを使用する

--- apps/blog/libs/contentfulClient.ts
+++ apps/blog/libs/contentfulClient.ts
@@ -8,3 +8,17 @@ export const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 });
+
+// Multiple versions of [slug] using page will be statically generated
+// using the `params` returned by `generateStaticParams`
+// https://nextjs.org/docs/app/api-reference/functions/generate-static-params
+export async function generateStaticParams() {
+  const queryOptions = {
+    content_type: "pageBlogPost",
+    select: "fields.slug",
+  };
+  const articles = await client.getEntries(queryOptions);
+  return articles.items.map((article) => ({
+    slug: article.fields.slug,
+  }));
+}
