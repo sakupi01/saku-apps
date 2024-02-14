309738d1467776afa39efa40d5856a701523ee66
saku-1101 <sakupi1101@outlook.jp>
2024-02-14 16:57:27 +0900

    chore: gh-pages branchのデプロイを禁止

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "${VERCEL_GIT_COMMIT_REF}" =~ ^(gh-pages)$ ]] ; then
+if [[ "${VERCEL_GIT_COMMIT_REF}" = "gh-pages" ]] ; then
   # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
@@ -12,3 +12,4 @@ else
   echo "✅ - Build can proceed"
   exit 1;
 fi
+
