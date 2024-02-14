637de84421d1548ad8409bb67ef3908e7656a8f5
saku-1101 <saku-1101@users.noreply.github.com>
2024-02-14 07:55:20 +0000

    deploy: 6cf4e7f48607dfb45acb987babaa6aa1aea9aa5a

--- assets/test.stories-DZaoDrQx.js
+++
@@ -1,3 +0,0 @@
-const s=({children:o})=>React.createElement("div",null,React.createElement("h1",null,"Test Component"),o);s.__docgenInfo={description:"",methods:[],displayName:"Test",props:{children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const c={title:"Test",component:s,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},e={args:{}};var t,a,r;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {}
-}`,...(r=(a=e.parameters)==null?void 0:a.docs)==null?void 0:r.source}}};const n=["Default"];export{e as Default,n as __namedExportsOrder,c as default};
