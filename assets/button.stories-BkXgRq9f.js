073bc59b8abd1b000952798907e4de1da017c2a4
saku-1101 <saku-1101@users.noreply.github.com>
2024-02-24 06:43:17 +0000

    deploy: 23c60c8a4fc7379dc9a9fd8125d965de948781f1

--- assets/button.stories-BkXgRq9f.js
+++
@@ -1,6 +0,0 @@
-import{j as c}from"./jsx-runtime-DRTy3Uxn.js";import{c as m}from"./clsx-B-dksMZM.js";import"./index-BBkUAzwr.js";const s=({children:o,className:n,appName:p})=>c.jsx("button",{type:"submit",className:m("px-4 py-2 text-black bg-green-500 rounded-md shadow-md hover:bg-blue-500",n),onClick:()=>alert(`Hello from your ${p} app!`),children:o});s.__docgenInfo={description:"",methods:[],displayName:"Button",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},appName:{required:!0,tsType:{name:"string"},description:""}}};const l={title:"Button",component:s,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},e={args:{children:"Button",appName:"app"}};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "Button",
-    appName: "app"
-  }
-}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,l as default};
--- assets/test.stories-XML0bU4v.js
+++
@@ -1,3 +0,0 @@
-import{j as s}from"./jsx-runtime-DRTy3Uxn.js";import"./index-BBkUAzwr.js";const o=({children:c})=>s.jsxs("div",{children:[s.jsx("h1",{children:"Test Component"}),c]});o.__docgenInfo={description:"",methods:[],displayName:"Test",props:{children:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""}}};const p={title:"Test",component:o,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},e={args:{}};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {}
-}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const i=["Default"];export{e as Default,i as __namedExportsOrder,p as default};
