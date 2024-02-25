f358258d3984bc5c43494f36db44030e43a60633
saku-1101 <sakupi1101@outlook.jp>
2024-02-25 17:33:47 +0900

    chore: style back button and create button component

--- packages/ui/src/components/button/button.stories.ts
+++ packages/ui/src/components/button/button.stories.ts
@@ -16,7 +16,44 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: "Button",
-    appName: "app",
+    intent: "primary",
+    size: "medium",
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    intent: "secondary",
+    size: "medium",
+  },
+};
+
+export const Subtle: Story = {
+  args: {
+    intent: "subtle",
+    size: "medium",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    intent: "ghost",
+    size: "medium",
+  },
+};
+
+export const SquareIcon: Story = {
+  args: {
+    intent: "square-icon",
+    size: "square",
+    children: "🌟",
+  },
+};
+
+export const RoundIcon: Story = {
+  args: {
+    intent: "round-icon",
+    size: "square",
+    children: "🌟",
   },
 };
