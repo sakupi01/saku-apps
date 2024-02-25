ca4f266f0655e14bd2021ac604482d5a6b09da20
saku-1101 <saku-1101@users.noreply.github.com>
2024-02-25 08:36:32 +0000

    deploy: c955f3fa2d68312ee58556650875a0fa8671b0be

--- assets/button.stories-ClVio5MG.js
+++
@@ -1,6 +0,0 @@
-import{j as i}from"./jsx-runtime-DRTy3Uxn.js";import{c as m}from"./clsx-B-dksMZM.js";import"./index-BBkUAzwr.js";const s=({children:o,className:n,onClick:p})=>i.jsx("button",{type:"submit",className:m("px-4 py-2 text-base bg-button-primary rounded-md shadow-md hover:bg-button-primary-hover hover:text-white",n),onClick:p,children:o});s.__docgenInfo={description:"",methods:[],displayName:"Button",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""}}};const l={title:"Button",component:s,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},e={args:{children:"Button",appName:"app"}};var t,r,a;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    children: "Button",
-    appName: "app"
-  }
-}`,...(a=(r=e.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const g=["Default"];export{e as Default,g as __namedExportsOrder,l as default};
