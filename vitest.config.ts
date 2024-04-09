869a5bc7205d62cf75d0b66cae42eca74d5b9b55
saku-1101 <sakupi1101@outlook.jp>
2024-04-09 22:43:35 +0900

    chore: vitestの管轄をルートに移す

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [codspeedPlugin()],
+  test: {
+    benchmark: {
+      include: ["apps/blog/**/*.bench.(js|ts)"],
+    },
+  },
 });
