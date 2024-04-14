a3dc8e2b072878481302ed87e16dd0845de7d19e
saku-1101 <sakupi1101@outlook.jp>
2024-04-14 11:53:08 +0900

    chore: e2e準備

--- apps/blog/playwright.config.ts
+++ apps/blog/playwright.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "next/experimental/testmode/playwright";
 
 export default defineConfig({
+  testDir: "test/e2e",
   webServer: {
-    command: "bun run dev -- --experimental-test-proxy",
+    command: "NODE_ENV='test' bun run dev -- --experimental-test-proxy",
     url: "http://localhost:3000",
   },
 });
