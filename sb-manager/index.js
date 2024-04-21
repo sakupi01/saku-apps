89d5e47c0cb7136e9762c54293946e5b69d1bc14
saku-1101 <saku-1101@users.noreply.github.com>
2024-04-21 04:13:49 +0000

    deploy: 5dde08d07be06c12dd42f3ddba3a25a3d198e893

--- sb-manager/index.js
+++
@@ -1,1 +0,0 @@
-import{Provider,Root,renderStorybookUI}from"./chunk-ZJDY5DQC.js";import"./chunk-SJTLHNHW.js";import"./chunk-7EIPTVVO.js";import"./chunk-W2OTLW5W.js";import"./chunk-AMRE34KO.js";import"./chunk-3BK6Y7TM.js";export{Provider,Root,renderStorybookUI};
