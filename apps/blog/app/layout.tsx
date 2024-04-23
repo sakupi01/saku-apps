909bfdbac688ea79ff868a37f6157af843936425
saku-1101 <sakupi1101@outlook.jp>
2024-04-23 22:07:05 +0900

    chore: update icon

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -57,11 +57,6 @@ export default function RootLayout({
   const date = new Date();
   return (
     <html lang="en">
-      <link
-        rel="icon"
-        // biome-ignore lint/style/noUnusedTemplateLiteral
-        href={`data:image/svg+xml,\<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22\>\<text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:100px;%22\>🌸\</text\>\</svg\>`}
-      />
       <body className={inter.className} id="scrollArea">
         <NavigationBar />
         {children}
