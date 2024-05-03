fe1a203f9115293025c6a8bd82bab67d41c47b14
saku-1101 <sakupi1101@outlook.jp>
2024-05-04 00:12:53 +0900

    wip

--- apps/me/app/routes/index.tsx
+++ apps/me/app/routes/index.tsx
@@ -2,6 +2,7 @@ import { createRoute } from "honox/factory";
 import FlowArea from "@/islands/portal/flowarea";
 
 export default createRoute((c) => {
+  // Honoのハンドラを返す
   return c.render(<FlowArea />, {
     head: {
       title: "saku's Portfolio - Home",
