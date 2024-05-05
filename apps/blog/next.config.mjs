d223ff9899bd136faee8f7eb18b4d2f8a1753846
saku-1101 <sakupi1101@outlook.jp>
2024-05-05 23:33:59 +0900

    chore: serverless functions dir resolution setting

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -5,7 +5,17 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ["@repo/ui"],
   headers,
-  nftTracing: true,
+  experimental: {
+    // includes files from the monorepo base two directories up
+    outputFileTracingRoot: path.join(process.cwd(), "../../"),
+    turbotrace: {
+      // control if the log of turbotrace should contain the details of the analysis, default is `false`
+      logDetail: true,
+      // if there is `process.cwd()` expression in your code, you can set this option to tell `turbotrace` the value of `process.cwd()` while tracing.
+      // for example the require(process.cwd() + '/package.json') will be traced as require('/path/to/cwd/package.json')
+      processCwd: "apps/blog",
+    },
+  },
 };
 
 export default nextConfig;
