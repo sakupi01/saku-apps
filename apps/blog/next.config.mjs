adec1b428c70b23e9ba3753e5be9ca5c2571f44e
saku-1101 <sakupi1101@outlook.jp>
2024-05-05 23:47:54 +0900

    chore: hide trace everything

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -10,8 +10,6 @@ const nextConfig = {
     // includes files from the monorepo base two directories up
     outputFileTracingRoot: path.join(process.cwd(), "../../"),
     turbotrace: {
-      // control if the log of turbotrace should contain the details of the analysis, default is `false`
-      logDetail: true,
       // if there is `process.cwd()` expression in your code, you can set this option to tell `turbotrace` the value of `process.cwd()` while tracing.
       // for example the require(process.cwd() + '/package.json') will be traced as require('/path/to/cwd/package.json')
       processCwd: ".",
