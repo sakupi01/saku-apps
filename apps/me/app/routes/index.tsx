cbba16d40b15d53fdcfe4d0dd2af2a0d70f108a9
saku-1101 <sakupi1101@outlook.jp>
2024-05-06 12:18:52 +0900

    chroe: change

--- apps/me/app/routes/index.tsx
+++ apps/me/app/routes/index.tsx
@@ -10,7 +10,7 @@ export default createRoute((c) => {
   const name = c.req.query("name") ?? "Hono";
   return c.render(
     <div class={className}>
-      <h1>Hello, Hono!</h1>
+      <h1>Hello, {name}!</h1>
       <Counter />
     </div>,
     { title: name },
