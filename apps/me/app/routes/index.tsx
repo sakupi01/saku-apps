4e8ad2db4265be316bcbe1bafb7330efc302ff8c
saku-1101 <sakupi1101@outlook.jp>
2024-05-06 11:36:08 +0900

    chore: change

--- apps/me/app/routes/index.tsx
+++ apps/me/app/routes/index.tsx
@@ -1,18 +1,18 @@
-import { css } from 'hono/css'
-import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
+import { css } from "hono/css";
+import { createRoute } from "honox/factory";
+import Counter from "../islands/counter";
 
 const className = css`
   font-family: sans-serif;
-`
+`;
 
 export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
+  const name = c.req.query("name") ?? "Hono";
   return c.render(
     <div class={className}>
-      <h1>Hello, {name}!</h1>
+      <h1>Hello, Hono!</h1>
       <Counter />
     </div>,
-    { title: name }
-  )
-})
+    { title: name },
+  );
+});
