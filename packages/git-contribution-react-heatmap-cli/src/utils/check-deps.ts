fd370c6f369b7092c942d7c2d1ab28bf9ed34e77
saku-1101 <sakupi1101@outlook.jp>
2024-06-07 00:12:58 +0900

    🐞fix: tailwindインストール状況読み取りの修正

--- packages/git-contribution-react-heatmap-cli/src/utils/check-deps.ts
+++ packages/git-contribution-react-heatmap-cli/src/utils/check-deps.ts
@@ -2,13 +2,10 @@ import fs from "node:fs";
 import path from "node:path";
 import { join } from "node:path";
 /*
- * Check if the required dependencies are installed in the given project's package.json
- * Dynamic require of
+ * Check if tailwindcss is there checking if tailwind.config.ts|js exists
  */
 export const checkTailwindCSS = async () => {
-  const path = join(process.cwd(), "package.json");
-  const data = fs.readFileSync(path, "utf8");
-  const packageJson = JSON.parse(data);
-  const devDependencies = Object.keys(packageJson.devDependencies || {});
-  return devDependencies.includes("tailwindcss");
+  const path2Config = join(process.cwd(), "tailwind.config");
+  const exists = fs.existsSync(`${path2Config}.${"ts" || "js"}`);
+  return exists;
 };
