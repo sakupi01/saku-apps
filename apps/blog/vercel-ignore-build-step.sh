c21bbd97342f3f557347298d4e4056cd16ec836f
saku-1101 <sakupi1101@outlook.jp>
2024-06-16 22:57:28 +0900

    👷chore: fix regexp

--- apps/blog/vercel-ignore-build-step.sh
+++ apps/blog/vercel-ignore-build-step.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ $VERCEL_GIT_COMMIT_REF =~ ^renovete/.* || $VERCEL_GIT_COMMIT_REF =~ ^gh-pages ]] ; then
+if [[ $VERCEL_GIT_COMMIT_REF =~ ^(renovate|gh-pages) ]] ; then
   # Don't build
   echo "🛑 - Build cancelled"
   exit 0;
