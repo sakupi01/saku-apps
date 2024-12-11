247ce21d8533be71d631a754c397f48219b27f8b
sakupi01 <sakupi01@users.noreply.github.com>
2024-12-11 03:50:52 +0000

    deploy: 0473ccc4fb22a44ce55359adb192a66aecb970f8

--- assets/divider.stories-8CH6bfgz.js
+++
@@ -1,3 +0,0 @@
-import{D as o}from"./divider-CEoC2itV.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./clsx-B-dksMZM.js";const i={title:"Divider",component:o,tags:["autodocs"],parameters:{layout:"fullscreen"},argTypes:{}},r={args:{}};var e,a,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {}
-}`,...(t=(a=r.parameters)==null?void 0:a.docs)==null?void 0:t.source}}};const n=["Default"];export{r as Default,n as __namedExportsOrder,i as default};
